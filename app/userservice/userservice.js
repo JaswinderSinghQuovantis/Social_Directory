5d28de2650aa9fd2c45f64a66693ece3a04b5e26
JaswinderSinghQuovantis <jaswinder.singh@quovantis.com>
2022-03-08 11:35:08 +0530

    Added : Login feature is Added in Social Directory

--- app/userservice/userservice.js
+++ app/userservice/userservice.js
@@ -1,11 +1,13 @@
 import user from '../model/model.js'
+import helper from '../helper/global.helper.js'
+import bcrypt from 'bcrypt'
 class Service {
     /**
      * @description: Adds data to the database
      * @param {object} userDetails
      * @param {function} Promise
      */
-    register = async (userDetails) => {
+    Register = async (userDetails) => {
             const newUser = new user();
             newUser.email = userDetails.email;
             newUser.password = userDetails.password;
@@ -15,9 +17,34 @@ class Service {
                 return userResult
             }
            catch(error){
-            return error
+               console.log("333",error);
+             return error
            }
+    }
+    /**
+	 * @description Find user with emailId
+     * @param loginData
+	 * @method findOne will find user with specific emailId
+	 */
 
+    Login = async (loginData)=>{
+       const checkuserexist = await user.findOne({email:loginData.email})
+       if(checkuserexist){
+           try{
+           const hash = await bcrypt.compare(loginData.password,checkuserexist.password)
+           if(hash){
+               const token = helper.UserToken(loginData)
+               if(token){
+                   return token
+               }
+           }else if (!hash){
+               return "Invalid Credential"
+           }
+        }catch(error){
+            return error
+        }
+       }else if(!checkuserexist)
+       return checkuserexist
     }
 }
 export default new Service()
